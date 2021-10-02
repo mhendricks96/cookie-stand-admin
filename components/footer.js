d1d5ec738abc4e416f6564b3bb75522bff1199e2
mhendricks96 <mhendricks96@yahoo.com>
2021-10-02 11:54:09 -0700

    started on lab during demo. broke out most of index into componets

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return(
+    <div>
+      <footer className="p-4 bg-green-500 mt-8">
+        <p>©2021</p>
+      </footer>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { useState } from 'react'
+import Footer from '../components/footer'
+import Header from '../components/header'
+import Main from '../components/main'
 
 export default function Home() {
 
@@ -27,79 +30,9 @@ export default function Home() {
         <title>Cookie Stand Admin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className="flex items-center justify-between p-4 bg-green-500 text-black-100 text-3xl mb-8">
-        <h1>Cookie Stand Admin</h1>
-        <Link href="/about_us">
-          <a href="about_us">About Us</a>       
-        </Link>
-      </header>
-      <main>
-      <form onSubmit={createStandHandler} className="bg-green-200 p-8 w-9/12 rounded-md items-center mx-auto">
-          <h3 className="text-center pb-2">Create Cookie Stand</h3>
-          <ul>
-            <li><label className="text-xs w-full">Location<input name="location" className="flex-auto w-5/6"/></label></li>
-            <li className="p-4">
-              <ul className="inline-block p-2">
-                <li>
-                  <label className="text-xs">Minimum Customers per Hour</label>
-                </li>
-                <li>
-                  <input name="min_per_hour"/>
-                </li>
-              </ul>
-              <ul className="inline-block p-2">
-                <li>
-                  <label className="text-xs">Maximum Customers per Hour</label>
-                </li>
-                <li>
-                  <input name="max_per_hour"/>
-                </li>
-              </ul>
-              <ul className="inline-block p-2">
-                <li>
-                  <label className="text-xs">Average Cookies per Sale</label>
-                </li>
-                <li>
-                  <input name="cookies_per_sale"/>
-                </li>
-              </ul>
-
-              <button className="px-2 py-01 bg-green-500 w-24 h-10">Create</button>
-            </li>
-          </ul>
-        </form>
-        <div className="text-center mt-7">
-          <p>Report Table Coming Soon...</p>
-          <table className="mx-auto w-1/2">
-            <thead>
-              <tr>
-                <th>Stand No.</th>
-                <th> Location </th>
-                <th> minCustomers </th>
-                <th> maxCustomers </th>
-                <th> avgCookies </th>
-              </tr>
-            </thead>
-            <tbody>
-              {cookieStands.map(item => {
-                return (
-                  <tr>
-                    <td>{item.id}</td>
-                    <td>{item.location}</td>
-                    <td>{item.minCustomers}</td>
-                    <td>{item.maxCustomers}</td>
-                    <td>{item.avgCookies}</td>
-                  </tr>
-                )
-              })}
-              
-            </tbody>
-          </table>
-        </div>
-      </main>
-      <footer className="p-4 bg-green-500 mt-8">
-        <p>©2021</p>
-      </footer>
+      <Header />
+      <Main createStandHandler={createStandHandler} cookieStands={cookieStands}/>
+      <Footer />
     </div>
   )
 }
